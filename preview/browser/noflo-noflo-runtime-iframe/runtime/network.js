568e4907018a43af7c99c96bc4f36bc439b9cd23
Forrest Oliphant <forrest@sembiki.com>
2013-12-28 17:49:08 +0200

    lost libs

---
+++ preview/browser/noflo-noflo-runtime-iframe/runtime/network.js
@@ -0,0 +1,56 @@
+(function (context) {
+  var noflo = context.require('noflo');
+  var Base = context.require('noflo-noflo-runtime-base');
+
+  var Iframe = function (options) {
+    if (!options) {
+      options = {};
+    }
+
+    if (options.catchExceptions) {
+      context.onerror = function (err) {
+        this.send('network', 'error', {
+          message: err.toString()
+        }, {
+          href: this.context ? this.context.href : context.parent.location.href
+        });
+        return true;
+      }.bind(this);
+    }
+
+    this.prototype.constructor.apply(this, arguments);
+    this.receive = this.prototype.receive;
+  };
+  Iframe.prototype = Base;
+  Iframe.prototype.send = function (protocol, topic, payload, ctx) {
+    if (payload instanceof Error) {
+      payload = {
+        message: payload.toString()
+      };
+    }
+    if (this.context) {
+      ctx = this.context;
+    }
+    context.parent.postMessage({
+      protocol: protocol,
+      command: topic,
+      payload: payload
+    }, ctx.href);
+  };
+  var runtime = new Iframe({
+    catchExceptions: true
+  });
+
+  context.addEventListener('message', function (message) {
+    if (!message.data.protocol) {
+      return;
+    }
+    if (!message.data.command) {
+      return;
+    }
+    runtime.receive(message.data.protocol, message.data.command, message.data.payload, {
+      href: message.origin
+    });
+  });
+
+})(window);
